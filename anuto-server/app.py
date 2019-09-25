6c70aac8d17852b616ffb16fd40f9924a2d7ffe1
Carlo Fragni <carlo@cartesi.io>
2019-09-25 11:37:42 -0300

    Add api/ to the start of all routes in the server

--- anuto-server/app.py
+++ anuto-server/app.py
@@ -6,7 +6,7 @@ from resources.scores import Scores
 cors = CORS(allow_all_origins=True)
 
 api = falcon.API(middleware=[cors.middleware])
-api.add_route('/tournaments', Tournaments())
-api.add_route('/tournaments/{tour_id}', Tournaments(), suffix='single')
-api.add_route('/tournaments/{tour_id}/scores/my', Scores(), suffix='my')
-api.add_route('/tournaments/{tour_id}/scores/{player_id}', Scores())
+api.add_route('/api/tournaments', Tournaments())
+api.add_route('/api/tournaments/{tour_id}', Tournaments(), suffix='single')
+api.add_route('/api/tournaments/{tour_id}/scores/my', Scores(), suffix='my')
+api.add_route('/api/tournaments/{tour_id}/scores/{player_id}', Scores())
