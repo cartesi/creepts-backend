094cdf2315e39ba53d94c963fb5213b61bd19a02
Carlo Fragni <carlo@cartesi.io>
2019-09-25 20:14:03 -0300

    Adjust falcon CORS middleware
    Explicitly allowing get, put and post methods and accepting
    content_type header

--- anuto-server/app.py
+++ anuto-server/app.py
@@ -3,7 +3,10 @@ from falcors import CORS
 from resources.tournaments import Tournaments
 from resources.scores import Scores
 
-cors = CORS(allow_all_origins=True)
+cors = CORS(
+    allow_all_origins=True,
+    allow_methods_list=['GET', 'PUT', 'POST'],
+    allow_headers_list=['content-type'])
 
 api = falcon.API(middleware=[cors.middleware])
 api.add_route('/api/tournaments', Tournaments())
