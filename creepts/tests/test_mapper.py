8fb4a139ee4a2b3a80d6cb5d0fff5ccde287826c
Carlo Fragni <carlo@cartesi.io>
2020-01-08 03:13:53 -0300

    Update tests

--- creepts/tests/test_mapper.py
+++ creepts/tests/test_mapper.py
@@ -6,7 +6,7 @@ from creepts.dispatcher.contract import Contract
 from creepts.mapping.mapper import Mapper
 from creepts.model.tournament import TournamentPhase
 
-TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'instance.json')
+TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'instance_samples/instance_step_11_pretty.json')
 
 class TestMapper(unittest.TestCase):
 
@@ -15,15 +15,15 @@ class TestMapper(unittest.TestCase):
 
         with open(TESTDATA_FILENAME) as json_file:
             json_data = json.load(json_file)
-        
+
         dapp = Contract(json_data)
         tournament = mapper.to_tournament(dapp)
 
-        self.assertEqual(tournament.id, "0x59d956d5eeb2f7d93453ce028d1d4a0bee46543a")
+        self.assertEqual(tournament.id, 11)
         self.assertEqual(tournament.phase, TournamentPhase.ROUND)
         self.assertEqual(tournament.currentRound, 1)
-        self.assertEqual(tournament.lastRound, 1)
-        self.assertEqual(tournament.deadline, datetime(2019, 10, 24, 22, 12, 17))
+        self.assertEqual(tournament.lastRound, 0)
+        self.assertEqual(tournament.deadline, datetime(2019, 12, 26, 21, 49, 37))
         # self.assertEqual(tournament.currentOpponent, None)
         self.assertEqual(tournament.winner, None)
 
