eaf8ac3cb980733e03a961cd7f737a40e39a7fad
Carlo Fragni <carlo@cartesi.io>
2020-01-17 03:18:28 -0300

    Update test assert value and setup

--- creepts/tests/test_mapper.py
+++ creepts/tests/test_mapper.py
@@ -1,7 +1,8 @@
 import os
+
+os.environ['ACCOUNT_ADDRESS'] = "0x760841c050d07d3f74139154284d1cd8b5afa9c6"
 import unittest
 import json
-from datetime import datetime
 from creepts.dispatcher.contract import Contract
 from creepts.mapping.mapper import Mapper
 from creepts.model.tournament import TournamentPhase
@@ -23,7 +24,7 @@ class TestMapper(unittest.TestCase):
         self.assertEqual(tournament.phase, TournamentPhase.ROUND)
         self.assertEqual(tournament.currentRound, 1)
         self.assertEqual(tournament.lastRound, 0)
-        self.assertEqual(tournament.deadline, datetime(2019, 12, 26, 21, 49, 37))
+        self.assertEqual(tournament.deadline, '2019-12-26T21:49:37')
         # self.assertEqual(tournament.currentOpponent, None)
         self.assertEqual(tournament.winner, None)
 
