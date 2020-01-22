9f964b91f6e14d7864078cafc6c8fe010c6df26f
Carlo Fragni <carlo@cartesi.io>
2020-01-21 23:45:39 -0300

    Improve cli tool to log dispatcher states
    Now the tool queries the dispatcher for active instances to make
    sure that the instance it tries to track exists and it's active

--- creepts/tests/log_dispatcher_outputs.py
+++ creepts/tests/log_dispatcher_outputs.py
@@ -2,28 +2,47 @@ import requests
 import time
 import json
 
-SLEEP_TIME=0.1
+QUERY_INDEX = 0
+SLEEP_TIME = 0.1
 DISPATCHER_URL = "http://localhost:3001"
 BASE_FILENAME = "instance_step_{}.json"
 
 last_resp = None
-index = 0
+step_index = 0
+
+def get_instance_indexes():
+    headers = {'Content-type': 'application/json'}
+    response = requests.get(DISPATCHER_URL, headers=headers)
+    if (response.status_code == 200):
+        return response.json()
+    else:
+        return None
 
 while True:
     time.sleep(SLEEP_TIME)
     try:
-        response = requests.get(DISPATCHER_URL, json={"Instance": 0})
+        instances_resp = get_instance_indexes()
+        if instances_resp:
+            if QUERY_INDEX in [int(idx) for idx in instances_resp]:
+                response = requests.get(DISPATCHER_URL, json={"Instance": QUERY_INDEX})
+            else:
+                print("{} - Index {} not in active instances list".format(time.ctime(), QUERY_INDEX))
+                continue
+        else:
+            print("{} - Index {} not in active instances list".format(time.ctime(), QUERY_INDEX))
+            continue
+
     except Exception as e:
-        print(e)
+        print("{} - {}".format(time.ctime(), e))
         continue
 
     if response.status_code != 200:
-        print("Not 200 status code")
+        print("{} - Not 200 status code".format(time.ctime()))
         continue
 
     if response.text != last_resp:
-        with open(BASE_FILENAME.format(index),'w') as out_file:
+        with open(BASE_FILENAME.format(step_index),'w') as out_file:
             out_file.write(json.dumps(response.json(), indent=4, sort_keys=True))
-            index += 1
-            print("Saved new dump, index is now {}".format(index))
+            step_index += 1
+            print("{} - Saved new dump, step index is now {}".format(time.ctime(), step_index))
             last_resp = response.text
