215d8c4e08753cd5b8965b27c0a2b64c2f6d00c1
Carlo Fragni <carlo@cartesi.io>
2020-01-22 22:18:25 -0300

    Update get instance cli test tool
    Forcing the instance index to be sent as an int

--- creepts/tests/httpie_test_get_instance.sh
+++ creepts/tests/httpie_test_get_instance.sh
@@ -1,6 +1,6 @@
 if [ $# -eq 1 ]
 then
-    http get localhost:3001 content-type:application/json Instance=$1
+    echo "{\"Instance\":$1}" | http get localhost:3001 content-type:application/json
 else
     echo "Usage:./httpie_test_get_instance.sh <instance_number>"
 fi
